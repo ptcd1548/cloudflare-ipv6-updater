4e25a1341d7f5300a3e434029adabbc67133b60a
ptcd <127763796+ptcd1548@users.noreply.github.com>
2023-03-16 11:55:19 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set your Cloudflare API credentials and subdomain name
-EMAIL="your_email@example.com"
-API_KEY="your_global_api_key"
-ZONE_ID="your_zone_id"
-SUBDOMAIN="subdomain.example.com"
+EMAIL="your_email@example.com" # Put your Cloudflare account email
+API_KEY="your_global_api_key" # Put your global API Key
+ZONE_ID="your_zone_id" # Put your Zone ID Here
+SUBDOMAIN="subdomain.example.com" # Put your subdomain here
 
 # Get the current IPv6 address of the machine
-IPV6=$(curl -s 'https://ipinfo.io/ip' | grep -oE '([a-f0-9]{0,4}:){7}[a-f0-9]{0,4}')
+IPV6=$(curl -s 'https://ipv6.whatismyip.akamai.com' | grep -oE '([a-f0-9]{0,4}:){7}[a-f0-9]{0,4}')
 
 # Get the DNS record ID of the subdomain
 RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=AAAA&name=${SUBDOMAIN}" \
